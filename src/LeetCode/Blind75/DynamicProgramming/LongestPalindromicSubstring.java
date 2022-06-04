aa92c8491cae8ca1d641d6ccbcfc65d25ddfa059
Dylan Pina <dillon_idgafos@yahoo.com>
2022-06-04 11:12:45 -0400

    Blind75 - Longest Palindromic Substring

---
+++ src/LeetCode/Blind75/DynamicProgramming/LongestPalindromicSubstring.java
@@ -0,0 +1,47 @@
+package LeetCode.Blind75.DynamicProgramming;
+/*
+ * PROBLEM: https://leetcode.com/problems/longest-palindromic-substring/
+ * SOLUTION: https://www.youtube.com/watch?v=XYQecbcd6_c
+ * BIG O: O(n^2)
+ */
+public class LongestPalindromicSubstring {
+
+    public static String longestPalindrome(String s) {
+        int strLength = s.length();
+        if (strLength < 2) return s;
+
+        int resultLength = 0;
+        String result = "";
+
+        for (int i = 0; i < s.length(); i++) {
+            // Odd length
+            int l = i, r = i;
+            while (l >= 0 && r < s.length() && s.charAt(l) == s.charAt(r)) {
+                if ((r - l + 1) > resultLength) {
+                    result = s.substring(l,r+1);
+                    resultLength = r - l + 1;
+                }
+                l -= 1;
+                r += 1;
+            }
+
+            // Even length
+            l = i;
+            r = i + 1;
+            while (l >= 0 && r < s.length() && s.charAt(l) == s.charAt(r)) {
+                if ((r - l + 1) > resultLength) {
+                    result = s.substring(l, r+1);
+                    resultLength = r - l + 1;
+                }
+                l -= 1;
+                r += 1;
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(longestPalindrome("babad"));
+        System.out.println(longestPalindrome("cbbd"));
+    }
+}
