167499b232c61c9a2eca6028cc85493e59b958e6
Dylan Pina <dillon_idgafos@yahoo.com>
2022-06-18 16:53:13 -0400

    Blind75 - Reverse Bits

---
+++ src/LeetCode/Blind75/BitManipulation/ReverseBits.java
@@ -0,0 +1,22 @@
+package LeetCode.Blind75.BitManipulation;
+/**
+ * PROBLEM: https://leetcode.com/problems/reverse-bits/
+ * SOLUTION: https://www.youtube.com/watch?v=UcoN6UjAI64
+ * BIG O: O(1)
+ */
+public class ReverseBits {
+
+    public static int reverseBits(int n) {
+        int res = 0;
+
+        for (int i = 0; i < 32; i++) {
+            int bit = (n >> i) & 1;
+            res = res | (bit << (31 - i));
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(reverseBits(00000010100101000001111010011100));
+    }
+}
